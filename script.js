4b3b912deece6fe711748b96375288644dc34546
RohitPurandhara <rohitp25@hotmail.com>
2025-03-13 18:55:36 -0400

    basic javascript added

---
+++ script.js
@@ -0,0 +1,54 @@
+function validateForm() {
+    let x = document.forms["myForm"]["name"].value;
+    let y = document.forms["myForm"]["message"].value;
+    let z = document.forms["myForm"]["email"].value;
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (x == ""||y==""||z=="") {
+      alert("Please fill out all fields before sending a message");
+      return false;
+    }else{
+        if(pattern.test(z)==false){
+            alert("Please enter a valid email address before sending a message")
+        }else{
+            alert("You have sent a message!")
+        }
+    }
+
+
+}
+
+
+
+function showDetails(){
+    const x = document.getElementById("myDetails1");
+    x.open = true;
+
+}
+
+
+// function validateForm(){
+
+//     if (localStorage.getItem("name")==null||localStorage.getItem("message")==null||localStorage.getItem("email")==null){
+
+//         alert("Please fill out all fields before sending a message")
+    
+//     }
+
+// }
+
+
+// function valid(email) {
+//     const input = document.createElement('input');
+//     input.type = 'email';
+//     input.value = email;
+//     return input.checkValidity();
+// }
+
+// function emailValidation(){
+
+//     if (localStorage.getItem("email")){
+        
+//     }
+// }
+
+
