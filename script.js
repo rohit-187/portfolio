cfc0e3a543e56f52afb6948ff9672bfbd7251b88
RohitPurandhara <rohitp25@hotmail.com>
2025-04-21 17:08:31 -0400

    added images, skill section and added smooth scrolling

--- script.js
+++ script.js
@@ -102,3 +102,41 @@ function myFunction() {
   }
 }
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function myFunction2() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+function toggleVisibility(id) {
+  const section = document.getElementById(id);
+  section.style.display = section.style.display === 'block' ? 'none' : 'block';
+}
